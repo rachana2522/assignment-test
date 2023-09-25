9be89d1645488df90a44ceee2984c1a61c28f0ba
Rachana priyadarshni samal <77429844+rachana2522@users.noreply.github.com>
2023-09-25 18:35:07 +0530

    Update RunningScripts.java

--- Todaytest/src/com/cozentus/Test/RunningScripts.java
+++ Todaytest/src/com/cozentus/Test/RunningScripts.java
@@ -3,32 +3,86 @@ package com.cozentus.Test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class RunningScripts {
 	public static void main(String[] args) {
-        try {
-            Connection connection = DbUtil.getConnection();
-            Statement statement = connection.createStatement();
+        DbManipulation dbManipulation = new DbManipulation();
 
-            BufferedReader reader = new BufferedReader(new FileReader("cozentusdb.sql"));
-            String line;
-            StringBuilder query = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                query.append(line);
-                if (line.endsWith(";")) {
-                    statement.execute(query.toString());
-                    query.setLength(0);
+
+
+        // Insert a subject
+        boolean subjectInserted = dbManipulation.insertSubject("Mathematics");
+        if (subjectInserted) {
+            System.out.println("Subject inserted successfully.");
+        } else {
+            System.out.println("Subject insertion failed.");
+        }
+
+
+
+        // Insert a student
+        boolean studentInserted = dbManipulation.insertStudent("John Doe", 85.5f, "Mathematics");
+        if (studentInserted) {
+            System.out.println("Student inserted successfully.");
+        } else {
+            System.out.println("Student insertion failed.");
+        }
+
+
+
+        // Get subject by ID
+        ArrayList<Object> subjectData = dbManipulation.getSubjectById(1);
+        System.out.println("Subject data: " + subjectData);
+
+
+
+        // Get student by ID
+        ArrayList<Object> studentData = dbManipulation.getStudentById(1);
+        System.out.println("Student data: " + studentData);
+
+
+
+        // Get all subjects
+        //ResultSet subjectsResultSet = dbManipulation.getAllSubjects();
+        // Process the result set as needed
+        ResultSet subjectsResultSet = dbManipulation.getAllSubjects();
+        if (subjectsResultSet != null) {
+            try {
+                while (subjectsResultSet.next()) {
+                    int subjectId = subjectsResultSet.getInt("subject_id");
+                    String subjectName = subjectsResultSet.getString("subject_name");
+                    System.out.println("Subject ID: " + subjectId + ", Subject Name: " + subjectName);
                 }
+                subjectsResultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
+        }
 
-            System.out.println("Tables created successfully.");
-            
-          
-            statement.close();
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+
+
+        // Get all students
+       // ResultSet studentsResultSet = dbManipulation.getAllStudents();
+        // Process the result set as needed
+        ResultSet studentsResultSet = dbManipulation.getAllStudents();
+        if (studentsResultSet != null) {
+            try {
+                while (studentsResultSet.next()) {
+                    int studentId = studentsResultSet.getInt("student_id");
+                    String studentName = studentsResultSet.getString("student_name");
+                    float score = studentsResultSet.getFloat("score");
+                    int subjectId = studentsResultSet.getInt("subject_id");
+                    System.out.println("Student ID: " + studentId + ", Student Name: " + studentName +
+                            ", Score: " + score + ", Subject ID: " + subjectId);
+                }
+                studentsResultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
