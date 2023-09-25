65d86e908203f73acd337c577abb16a2511a31a2
Rachana priyadarshni samal <77429844+rachana2522@users.noreply.github.com>
2023-09-25 18:34:34 +0530

    Update DbUtil.java

--- Todaytest/src/com/cozentus/Test/DbUtil.java
+++ Todaytest/src/com/cozentus/Test/DbUtil.java
@@ -6,11 +6,13 @@ import java.sql.SQLException;
 
 public class DbUtil {
 	private static final String DB_URL = "jdbc:mysql://localhost:3306/assessment_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root@123";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root@123";
+
+
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 
 }
