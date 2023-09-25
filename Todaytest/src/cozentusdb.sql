074650f1ae380c557ddf9b0f9446edba6f0086e4
Rachana priyadarshni samal <77429844+rachana2522@users.noreply.github.com>
2023-09-25 18:35:59 +0530

    Update cozentusdb.sql

--- Todaytest/src/cozentusdb.sql
+++ Todaytest/src/cozentusdb.sql
@@ -1,15 +1,23 @@
-CREATE DATABASE assessment_db;
+CREATE DATABASE assessment_db; 
+
+
+ 
+
 USE assessment_db;
 
-CREATE TABLE subject (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(25)
+ 
+
+CREATE TABLE Subject (
+    subject_id INT AUTO_INCREMENT PRIMARY KEY,
+    subject_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE student (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(26),
+ 
+
+CREATE TABLE Student (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(255) NOT NULL,
     score FLOAT,
     subject_id INT,
-    FOREIGN KEY (subject_id) REFERENCES subject(id)
+    FOREIGN KEY (subject_id) REFERENCES Subject(subject_id)
 );
